57c443623de52167a5cb32eb7ccdbf40f33cc3e4
Kaldinn <danielskulimowski.biuro@gmail.com>
2024-01-16 16:05:03 +0100

    Complete configure flask server and react client

--- server/app.py
+++ server/app.py
@@ -1,18 +1,16 @@
 from flask import Flask, jsonify
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
-app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/api/data', methods=['GET'])
 def get_data():
     data = {
-        'name': 'John Doe',
+        'name': 'Daniel',
         'age': 30,
         'email': 'john.doe@example.com'
     }
     return jsonify(data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
