029ba97447230c87d519ce021128945a495fe446
Kaldinn <danielskulimowski.biuro@gmail.com>
2024-01-18 19:55:39 +0100

    try to use MUI

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
-
 import React, { useState } from 'react';
-import Navbar from './components/Navbar/Navbar';
-import SideMenu from './components/SideMenu/SideMenu';
-import Content from './components/Content/Content';
+import Nav from './components/Nav/Nav';
+
+
 
 function App() {
 
-  const [isMenuHide, setIsMenuHide] = useState(true)
-
-  const toggleMenu = (e) =>{
-    if(!isMenuHide){
-      setIsMenuHide(true)
-      console.log(isMenuHide)
-    }else{
-      setIsMenuHide(false)
-      console.log(isMenuHide)
-    }
-  }
-
   return (
     <main>
-      <Navbar 
-        toggleMenu={toggleMenu}
+      <Nav
       />
-      <Content
-        isMenuHide={isMenuHide}
-       />
     </main>
 
   );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Style from './global.css'
 import App from './App';
-
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { InputBase } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
