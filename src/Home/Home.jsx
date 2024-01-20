562bcf2b116fbb0f86e491f55c926100bb356fa7
Kaldinn <danielskulimowski.biuro@gmail.com>
2024-01-20 20:40:58 +0100

    Create reusable componnent for blogs

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,21 +1,18 @@
+import { Preview } from '@mui/icons-material'
 import React, { useState } from 'react'
+import BlogList from '../BlogList/BlogList'
 
 const Home = () => {
 
-
-  const [name, setName] = useState('Daniel')
-  const [age, setAge] = useState(25)
-
-  const handleClick = () =>{
-    setName('Natalia')
-    setAge(30)
-  }
-
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
   return (
     <div className='home'>
-        <h2>Homepage</h2>
-        <p>{name} is {age}</p>
-        <button onClick={handleClick}>Click</button>
+      <BlogList blogs={blogs} blogsTitle = "All Blogs!"/>
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} blogsTitle = "Mario's Blogs!"/>
     </div>
   )
 }
