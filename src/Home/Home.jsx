626c2f9a3253562f91f83a00b314c9c47b55fc57
Kaldinn <danielskulimowski.biuro@gmail.com>
2024-01-20 19:59:05 +0100

    Add style to components

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
+
+
+  const [name, setName] = useState('Daniel')
+  const [age, setAge] = useState(25)
+
+  const handleClick = () =>{
+    setName('Natalia')
+    setAge(30)
+  }
+
   return (
-    <home>
-        HomePage
-    </home>
+    <div className='home'>
+        <h2>Homepage</h2>
+        <p>{name} is {age}</p>
+        <button onClick={handleClick}>Click</button>
+    </div>
   )
 }
 
