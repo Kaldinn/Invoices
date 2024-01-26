ce41175b88b77d6a0e5338327e6df3125e222bd9
Kaldinn <danielskulimowski.biuro@gmail.com>
2024-01-26 21:36:58 +0100

    test commit

--- src/FormInvoices/FormInvoices.jsx
+++ src/FormInvoices/FormInvoices.jsx
@@ -15,38 +15,39 @@ import "../FormInvoices/FormInvoices.css"
 const FormInvoices = () => {
   return (
     <form className='add-invoice'>
-    <div className='contractor'>
-      <button> Choose Contractor </button>
-    </div>
-    <div className="date-fields">
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DemoContainer components={['DateTimePicker']}>
-          <DateTimePicker label="Invoice Date" views={['year','month','day']}/>
-          <DateTimePicker label="Sell Date" views={['year','month','day']}/>
-          <DateTimePicker label="Payment Date" views={['year','month','day']}/>
-          <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Payment Method</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            label="Payment Method"  >
-            <MenuItem value={10}>Card</MenuItem>
-            <MenuItem value={20}>Cash</MenuItem>
-          </Select>
-        </FormControl>
-        </DemoContainer>
-      </LocalizationProvider>
-    </div>
-     <div className="summary">
-        <ul>
-          <li>Summary: </li>
-          <li>VAT: </li>
-          <li>Netto: </li>
-          <li>Discount: </li>
-          <li>Margin: </li>
-          <li>Status: </li>
-        </ul>
-     </div>
+      <div className='contractor'>
+        <button> Choose Contractor </button>
+      </div>
+      <div className="date-fields">
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DemoContainer components={['DateTimePicker']}>
+            <DateTimePicker label="Invoice Date" views={['year','month','day']}/>
+            <DateTimePicker label="Sell Date" views={['year','month','day']}/>
+            <DateTimePicker label="Payment Date" views={['year','month','day']}/>
+            <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Payment Method</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              label="Payment Method"  >
+              <MenuItem value={10}>Card</MenuItem>
+              <MenuItem value={20}>Cash</MenuItem>
+              <MenuItem value={20}>Cash</MenuItem>
+            </Select>
+          </FormControl>
+          </DemoContainer>
+        </LocalizationProvider>
+      </div>
+      <div className="summary">
+          <ul>
+            <li>Summary: </li>
+            <li>VAT: </li>
+            <li>Netto: </li>
+            <li>Discount: </li>
+            <li>Margin: </li>
+            <li>Status: </li>
+          </ul>
+      </div>
     </form>
   )
 }
