0ce14d999c6d3b2aa131e601213cf1d1c0627760
Kaldinn <danielskulimowski.biuro@gmail.com>
2024-01-27 18:13:08 +0100

    Add a few cosmetic changes on invoice view

--- src/FormInvoices/FormInvoices.jsx
+++ src/FormInvoices/FormInvoices.jsx
@@ -7,6 +7,7 @@ import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
+import Button from '@mui/material/Button';
 import Select from '@mui/material/Select';
 
 
@@ -16,11 +17,18 @@ const FormInvoices = () => {
   return (
     <form className='add-invoice'>
       <div className='contractor'>
-        <button> Choose Contractor </button>
+        <Button variant="contained">Add Contractor</Button>
+        <div className='contractor-credentials'>
+          <ul>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>4</li>
+          </ul>
+        </div>
       </div>
       <div className="date-fields">
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DemoContainer components={['DateTimePicker']}>
             <DateTimePicker label="Invoice Date" views={['year','month','day']}/>
             <DateTimePicker label="Sell Date" views={['year','month','day']}/>
             <DateTimePicker label="Payment Date" views={['year','month','day']}/>
@@ -32,20 +40,18 @@ const FormInvoices = () => {
               label="Payment Method"  >
               <MenuItem value={10}>Card</MenuItem>
               <MenuItem value={20}>Cash</MenuItem>
-              <MenuItem value={20}>Cash</MenuItem>
             </Select>
           </FormControl>
-          </DemoContainer>
         </LocalizationProvider>
       </div>
       <div className="summary">
           <ul>
-            <li>Summary: </li>
-            <li>VAT: </li>
-            <li>Netto: </li>
-            <li>Discount: </li>
-            <li>Margin: </li>
-            <li>Status: </li>
+            <li>Summary: <p>23</p></li>
+            <li>VAT: <p>23</p></li>
+            <li>Netto: <p>23</p></li>
+            <li>Discount: <p>23</p></li>
+            <li>Margin: <p>23</p></li>
+            <li>Status: <p>23</p></li>
           </ul>
       </div>
     </form>
